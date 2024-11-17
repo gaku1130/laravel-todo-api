23d617ff5fd7d5142bb5fed97ae85e5fd08b8623
gaku1130 <gk.99prd3o@gmail.com>
2024-11-17 15:26:08 +0900

    add:swagerno

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try{
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                $user = Auth::user();
+                $token = Auth::user()->createToken('AccessToken')->plainTextToken;
+                return response()->json([
+                    'status' => 200,
+                    'data' => [
+                        'userId' => $user->id,
+                        'accessToken' => $token,
+                    ],
+                ], 200);
+            } else {
+                return response()->json([
+                    'error' => [
+                        'code' => 400,
+                            'message' => 'Request Parameter Error',
+                            'details' => 'The provided credentials are incorrect.',
+                    ],
+                ],400);
+            }
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => [
+                    'code' => 500,
+                    'message' => 'Server Error',
+                    'details' => 'An unexpected error occurred on the server. Please try again later or contact support if the issue persists.',
+                ],
+            ], 500);
+            }
+        }
+    }
+}
+
+    public function user(Request $request){
+        return response()->json(
+            [
+                $request->user()->name,
+                $request->user()->email,
+            ]
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'ログアウトしました。'], 200);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
