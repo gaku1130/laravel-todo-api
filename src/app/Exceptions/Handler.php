c7ee2279355efdc063719267bb504acafa5b6d49
gaku1130 <gk.99prd3o@gmail.com>
2024-12-08 15:35:23 +0900

    fix:Handler.php

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\QueryException;
 use Throwable;
+use App\Exceptions\CustomException;
+use Illuminate\Support\Facades\Log;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +29,24 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $exception)
+    {
+			// メールアドレスの一意制約違反の場合の処理
+			if ($exception instanceof QueryException && $exception->errorInfo[1] == 1062) {
+				throw new CustomException('Request Parameter Error', ['email' => ['The email address already exists']], 400);
+			}
+
+			if ($exception instanceof CustomException) {
+				return $exception->render();
+			}
+
+			throw new CustomException('Internal Server Error', ['error' => $exception->getMessage()], 500);
+
+			// return parent::render($request, $exception);
     }
 }
